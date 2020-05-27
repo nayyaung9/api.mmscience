5b01ac82b726daa05cacf4499ee90a95e5c61372
nayyaung9 <naygit.developer@gmail.com>
2020-05-27 16:34:09 +0630

    feat: user proffile done

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,18 +1,66 @@
 const Post = require('../models/Post');
 const Tag = require('../models/Tag');
+const User = require('../models/User');
+var cloudinary = require("cloudinary").v2;
+const CONFIG = require('../../config/db');
 
 exports.fetchUserPosts = async (req, res) => {
   const { id } = req.params;
   const posts = await Post.find({ user: id }).populate('user').populate('tags').sort([["_id", -1]]);
-  if(!posts) return res.status(404).send('posts not found');
+  if (!posts) return res.status(404).send('posts not found');
   return res.status(200).json({ succes: true, data: posts });
 }
 
 exports.fetchUserTags = async (req, res) => {
   const { id } = req.params;
-  console.log('id', id);
   const tags = await Tag.find({ user: id }).populate('user');
-  console.log(tags);
-  if(!tags) return res.status(404).send('tags not found');
+  if (!tags) return res.status(404).send('tags not found');
   return res.status(200).json({ succes: true, data: tags });
+}
+
+exports.fetchUserProfile = async (req, res) => {
+    const { unique } = req.params;
+    console.log(unique);
+    const user = await User.findOne({ uniqueId: unique });
+    if(!user) return res.status(4040).send('User Not Found');
+    return res.status(200).json({ succes: true, data: user });
+}
+
+exports.updateProfile = async (req, res) => {
+  const { unique } = req.params;
+  let image = req.app.locals.imgName;
+
+  let imgUploading = new Promise((resolve, reject) => {
+    cloudinary.config({
+      cloud_name: CONFIG.cloudinary.name,
+      api_key: CONFIG.cloudinary.api_key,
+      api_secret: CONFIG.cloudinary.api_secret,
+    });
+    cloudinary.uploader
+      .upload(`${CONFIG.root}/public/profile/${image}`, {
+        folder: "profile",
+        use_filename: true,
+        unique_filename: false
+      })
+      .then(function (image) {
+        let imgUrl = image.secure_url;
+        return resolve(imgUrl);
+      })
+      .catch(function (err) {
+        if (err) {
+          console.warn(err);
+        }
+      });
+  })
+
+  let imgUrl = await imgUploading;
+  console.log('1');
+  const user = await User.findOneAndUpdate(
+    { uniqueId: unique },
+    { $set: { fullname: req.body.fullname, avatar_url: imgUrl } },
+    { new: true },
+  );
+
+  if(!user) res.status(500).send('THere was a problem while updating Profile');
+  res.status(200).json({ success: true, data: user });
 }
--- app/routes/userRoute.js
+++ app/routes/userRoute.js
@@ -3,6 +3,31 @@
 const userController = require('../controllers/userController');
 const { catchError } = require('../libs/errorHandler');
 const verifyToken = require('../libs/verifyToken');
+const multer = require('multer');
+
+var imgName = "IMG";
+var date = new Date();
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/profile');
+  },
+  filename: (req, file, cb) => {
+    if (!file) {
+      cb(null, false);
+    }
+    else {
+      var imgDateName = date.getTime();
+      var fileSplit = file.originalname.split('.');
+      var fileExtension = fileSplit[fileSplit.length - 1];
+      imgName = imgName + "-" + imgDateName + '-mmscience' + '.' + fileExtension;
+      cb(null, imgName);
+    }
+  }
+});
+
+const uploadStore = multer({
+  storage
+});
 
 module.exports = app => {
   app
@@ -11,8 +36,22 @@ module.exports = app => {
   app
     .route('/api/user/:id/posts')
     .get(verifyToken, catchError(userController.fetchUserPosts));
-
   app
     .route('/api/user/:id/tags')
     .get(catchError(userController.fetchUserTags));
+  app
+    .route('/api/user/:unique')
+    .get(verifyToken, catchError(userController.fetchUserProfile));
+  app
+    .route('/api/user/:unique/update')
+    .put(
+      verifyToken,
+      uploadStore.any(),
+      function (req, res, next) {
+        req.app.locals.imgName = imgName;
+        imgName = "IMG";
+        next()
+      },
+      catchError(userController.updateProfile));
+
 }
