9ee837d7fe19d6a2e6d2dee032dec2743b4fa0cb
nayyaung9 <naygit.developer@gmail.com>
2020-05-29 16:47:40 +0630

    fix: fix image dir on server

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -39,7 +39,7 @@ exports.updateProfile = async (req, res) => {
       api_secret: CONFIG.cloudinary.api_secret,
     });
     cloudinary.uploader
-      .upload(`${CONFIG.root}/public/profile/${image}`, {
+      .upload(`../${CONFIG.root}/public/profile/${image}`, {
         folder: "profile",
         use_filename: true,
         unique_filename: false
