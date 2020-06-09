a9cabb88e339cd78af4abf8f55b2bd11a96d1561
nayyaung9 <naygit.developer@gmail.com>
2020-06-09 23:42:21 +0630

    fix: auth fixing on nodemailer

--- app/libs/mailer.js
+++ app/libs/mailer.js
@@ -1,24 +1,27 @@
-const nodemailer = require('nodemailer'),
-  config = require('../../config/db');
+const nodemailer = require("nodemailer"),
+  config = require("../../config/db");
 
 var transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: config.gmailAuth.user,
-    pass: config.gmailAuth.pass,
-  },
+    pass: config.gmailAuth.pass
+  }
 });
 
 module.exports = function(msgObj) {
-  console.log('aaaaa');
-  console.log('msg', msgObj);
-  msgObj.from = config.gmailAuth.name + ' ' + config.gmailAuth.user;
-  console.log("Mail Object ",msgObj);
+  console.log("aaaaa");
+  console.log("msg", msgObj);
+  msgObj.from = config.gmailAuth.name + " " + config.gmailAuth.user;
+  console.log("Mail Object ", msgObj);
   transporter.sendMail(msgObj, function(error, info) {
     if (error) {
       console.log(error);
     } else {
-      console.log('Sent');
+      console.log("Sent");
     }
   });
 };
